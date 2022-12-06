56d28065013b78e707bfa3a9d0e2ea2b86ce3133
Emmanuel Sanchez <fede@fede.website>
2022-12-06 14:47:29 -0600

    first commit

---
+++ app/src/main/java/com/example/oopdemo/Car.kt
@@ -0,0 +1,10 @@
+package com.example.oopdemo
+
+import android.util.Log
+
+class Car {
+
+    fun start(){
+        Log.i("MyTag", "Car is starting...")
+    }
+}
---
+++ app/src/main/java/com/example/oopdemo/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.oopdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val car = Car()
+        car.start()
+    }
+}
